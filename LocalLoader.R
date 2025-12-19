6983aac32154b8d946b43bfbb27f7d11713165da
gabrielriedel <62902831+gabrielriedel@users.noreply.github.com>
2025-12-19 03:22:38 -0600

    light data pre-processing

--- LocalLoader.R
+++ LocalLoader.R
@@ -2,15 +2,28 @@
 
 folder_path <- "/Users/gaberiedel/Downloads/Fall-25"
 
-data <- read_csv(folder_path)
-
-View(data)
-
 files <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
 
-all_data <- files %>%
-  map_dfr(read_csv)
+all_data <- files |>
+  map_dfr(read_csv) |>
+  filter(Batter != "Volmerding, Joshua") |>
+  mutate(Batter = case_when(
+    Batter == "Vonderhaar, Coco" ~ "VonderHaar, Coco",
+    Batter == "Spiridonoff, Gavin" ~ "Spirdonoff, Gavin",
+    TRUE ~ Batter
+  ))
 
-write.csv(all_data, "all_fall_25.csv", row.names=FALSE)
+write.csv(all_data, "app/data/all_fall_25.csv", row.names=FALSE)
 
-write.csv(cp_df, "cp_df.csv", row.names=FALSE)
+write.csv(cp_df, "app/data/cp_df.csv", row.names=FALSE)
+
+
+cp_fall <- read.csv("app/data/all_fall_25.csv")
+fall |>
+  filter(Pitcher == "Volmerding, Joshua" & TaggedPitchType == "Slider") |>
+  mutate(IsWhiff = if_else(PitchCall == 'StrikeSwinging', 1, 0),
+         IsSwing = if_else(PitchCall %in% c('StrikeSwinging', 'FoulBallNotFieldable', 'InPlay', 'FoulBallFieldable','FoulBall'), 1, 0)) |>
+  summarize(
+    num_whiff = sum(IsWhiff),
+    num_pitches = sum(IsSwing)
+  )
